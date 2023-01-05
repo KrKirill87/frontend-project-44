8f0bf45b7a1685e4935c0fd86e4bf0eb346cb780
KrKirill87 <fanofzenitkirill@gmail.com>
2023-01-06 00:31:15 +0300

    fix game-even

--- src/games/game-even.js
+++ src/games/game-even.js
@@ -1,24 +1,14 @@
-import readlineSync from 'readline-sync';
+import startGame from '../index.js';
 import getRandomInt from '../randomFunction.js';
 
-export default function evenGame() {
-  console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
-  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const rulesOfGame = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-  for (let i = 0; i < 3; i += 1) {
-    const max = 50;
-    const question = getRandomInt(max);
-    console.log(`Question: ${question}`);
-    const trueAnswer = (question % 2) === 0 ? 'yes' : 'no';
-    const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer !== trueAnswer) {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${trueAnswer}'.`);
-      console.log(`Let's try again, ${name}!`);
-      return;
-    }
-    console.log('Correct!');
-  }
-  console.log(`Congratulations, ${name}!`);
-}
+const setGame = () => {
+  const question = getRandomInt(1, 10);
+  const answer = (question % 2) === 0 ? 'yes' : 'no';
+  return [question, answer];
+};
+
+const evenGame = () => startGame(rulesOfGame, setGame);
+
+export default evenGame;
